1fc31cfd8e5bf42d575806a996d0d20194b80d52
Paula Klein <kleinpaula@ymail.com>
2016-06-29 18:43:53 +0200

    Created rest of Converters

---
+++ Implementierung/Client/GoApp/app/src/main/java/edu/kit/pse/client/goapp/converter/ParticipantConverter.java
@@ -0,0 +1,28 @@
+package main.java.edu.kit.pse.client.goapp.converter;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+
+import java.util.List;
+
+import main.java.edu.kit.pse.client.goapp.datamodels.Group;
+import main.java.edu.kit.pse.client.goapp.datamodels.Participant;
+
+/**
+ * Implements the Inetrface Converter<Group> and converts JSON-Strings to Groups
+ * Created by paula on 29.06.16.
+ */
+public class ParticipantConverter implements Converter<Participant> {
+    private Gson gson = new GsonBuilder().create();
+
+    @Override
+    public Participant deserialize(String jsonString) {
+        return gson.fromJson(jsonString, Participant.class);
+    }
+
+    @Override
+    public List<Participant> deserializeList(String jsonString) {
+        return gson.fromJson(jsonString,new TypeToken<List<Participant>>(){}.getType());
+    }
+}
