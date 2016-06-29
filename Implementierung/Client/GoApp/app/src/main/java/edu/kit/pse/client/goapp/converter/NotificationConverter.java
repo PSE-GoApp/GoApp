e9fc1949dbadb600e4edece193c787d561efe1b4
Paula Klein <kleinpaula@ymail.com>
2016-06-29 15:22:49 +0200

    uri_Builder begonnen

---
+++ Implementierung/Client/GoApp/app/src/main/java/edu/kit/pse/client/goapp/converter/NotificationConverter.java
@@ -0,0 +1,7 @@
+package main.java.edu.kit.pse.client.goapp.converter;
+
+/**
+ * Created by paula on 29.06.16.
+ */
+public class NotificationConverter {
+}
---
+++ Implementierung/Client/GoApp/app/src/main/java/edu/kit/pse/client/goapp/uri_builder/URI_LoginBuilder.java
@@ -0,0 +1,7 @@
+package main.java.edu.kit.pse.client.goapp.uri_builder;
+
+/**
+ * Created by paula on 29.06.16.
+ */
+public class URI_LoginBuilder {
+}
