97d049e78b8ba70202c276b2738d44a7c1d2a5cc
Paula Klein <kleinpaula@ymail.com>
2016-06-29 15:26:17 +0200

    uri_builder begonnen

--- Implementierung/Client/GoApp/app/src/main/java/edu/kit/pse/client/goapp/uri_builder/URI_LoginBuilder.java
+++ Implementierung/Client/GoApp/app/src/main/java/edu/kit/pse/client/goapp/uri_builder/URI_LoginBuilder.java
@@ -1,7 +1,41 @@
 package main.java.edu.kit.pse.client.goapp.uri_builder;
 
+import org.apache.http.client.utils.URIBuilder;
+
+import java.lang.ref.SoftReference;
+import java.net.URI;
+
 /**
  * Created by paula on 29.06.16.
  */
-public class URI_LoginBuilder {
+public class URI_LoginBuilder extends ClientURI_Builder {
+
+    private static final String servletAdd = "bla"; //we don't know it yet
+    private URIBuilder uribuilder;
+    private URI uri;
+
+    public URI_LoginBuilder() {
+        try {
+            uribuilder = new URIBuilder(serverAdd + servletAdd);
+        } catch(java.net.URISyntaxException syntaxException){
+            //handle it somehow
+        }
+
+    }
+
+    @Override
+    public void addParameter(String key, String value) {
+        uribuilder.addParameter(key, value);
+
+    }
+
+    @Override
+    public String getURI() {
+        try {
+            uri = uribuilder.build();
+        } catch (java.net.URISyntaxException syntaxException){
+            //handle it somehow
+        }
+        return uri.toString();
+    }
 }
