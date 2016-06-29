cfa8170e17b254f05ae497aedc58df377031d415
Paula Klein <kleinpaula@ymail.com>
2016-06-29 15:25:41 +0200

    Converter begonnen

--- Implementierung/Client/GoApp/app/src/main/java/edu/kit/pse/client/goapp/converter/Converter.java
+++ Implementierung/Client/GoApp/app/src/main/java/edu/kit/pse/client/goapp/converter/Converter.java
@@ -1,7 +1,25 @@
 package main.java.edu.kit.pse.client.goapp.converter;
 
+import java.util.List;
+
 /**
+ * The Converters who convert JSON-Strings to Objects have to implement this Interface.
  * Created by paula on 29.06.16.
  */
-public interface Converter {
+public interface Converter<T> {
+
+    /**
+     * Creates an Object T out of the given JSON-String
+     * @param jsonString String in the format of a JSON-String
+     * @return String as Object T
+     */
+    public T deserialize(String jsonString);
+
+    /**
+     * Creates a List of Objects T out of the given JSON-String
+     * @param jsonString String in the format of a JSON-String
+     * @return String as List<T>
+     */
+    public List<T> deserializeList(String jsonString);
+
 }
