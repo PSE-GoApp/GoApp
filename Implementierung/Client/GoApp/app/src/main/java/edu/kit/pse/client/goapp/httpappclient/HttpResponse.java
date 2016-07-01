a12a48e4e46f16e7bd9ff98b643620c495adc3af
Paula Klein <kleinpaula@ymail.com>
2016-07-01 21:32:29 +0200

    Angefangen, muss noch ClosableHttpResponse verstehen

--- Implementierung/Client/GoApp/app/src/main/java/edu/kit/pse/client/goapp/httpappclient/HttpResponse.java
+++ Implementierung/Client/GoApp/app/src/main/java/edu/kit/pse/client/goapp/httpappclient/HttpResponse.java
@@ -1,7 +1,19 @@
 package main.java.edu.kit.pse.client.goapp.httpappclient;
 
-/**
+import org.apache.http.client.methods.CloseableHttpResponse;
+
+/**This Class contains the responces of the Server, which are returned in all Methods of the HttpAppClient
  * Created by paula on 01.07.16.
  */
 public class HttpResponse {
+    private static CloseableHttpResponse response;
+
+    public HttpResponse(CloseableHttpResponse response){
+        this.response = response;
+    }
+
+    public int getStatusCode() {
+        //need to understand the ClosableHttpResponse first
+        return 0;
+    }
 }
