e5acd4660181fad87855c579d0872483f777126e
Paula Klein <kleinpaula@ymail.com>
2016-07-10 15:27:32 +0200

    fertig mit userservice

--- Implementierung/Client/GoApp/app/src/main/java/edu/kit/pse/client/goapp/service/UserService.java
+++ Implementierung/Client/GoApp/app/src/main/java/edu/kit/pse/client/goapp/service/UserService.java
@@ -1,7 +1,212 @@
-package main.java.edu.kit.pse.client.goapp.service;
+package edu.kit.pse.client.goapp.service;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.ResultReceiver;
+
+import org.apache.http.client.methods.CloseableHttpResponse;
+
+import java.io.IOException;
+
+import edu.kit.pse.client.goapp.CommunicationKeys;
+import edu.kit.pse.client.goapp.httpappclient.HttpAppClientDelete;
+import edu.kit.pse.client.goapp.httpappclient.HttpAppClientGet;
+import edu.kit.pse.client.goapp.httpappclient.HttpAppClientPost;
+import edu.kit.pse.client.goapp.httpappclient.HttpAppClientPut;
+import edu.kit.pse.client.goapp.uri_builder.URI_UserBuilder;
 
 /**
  * Created by paula on 10.07.16.
  */
-public class UserService {
+public class UserService extends IntentService{
+
+    //Konstruktor gibt den Service ein Namen, der fürs Testen wichtig ist.
+
+    public UserService() {
+        super("UserService");
+    }
+
+    /**
+     * Creates an IntentService.  Invoked by your subclass's constructor.
+     *
+     * @param name Used to name the worker thread, important only for debugging.
+     */
+    public UserService(String name) {
+        super(name);
+    }
+
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        String command = intent.getStringExtra(edu.kit.pse.client.goapp.CommunicationKeys.COMMAND);
+        switch (command) {
+            case "GET":
+                doGet(intent);
+                break;
+            case "DELETE":
+                doDelet(intent);
+                break;
+            case "PUT":
+                doPut(intent);
+                break;
+            case "POST":
+                doPost(intent);
+                break;
+            default:
+                break;
+        }
+    }
+
+    //Das Erstellen eines benutzers, Änderungen der Benutzerinformationen, Löschen eines Benutzers
+
+    private void doGet (Intent intent) //throws  IOException
+    {
+        String jasonString = null;
+        CloseableHttpResponse closeableHttpResponse = null;
+
+        final ResultReceiver resultReceiver = intent.getParcelableExtra(CommunicationKeys.RECEICER);
+
+        Bundle bundle = new Bundle();
+        bundle.putString(CommunicationKeys.COMMAND, CommunicationKeys.GET);
+        bundle.putString(CommunicationKeys.SERVICE, CommunicationKeys.FROM_USER_SERVICES);
+
+        // if there no User Id in the Extra returns -1
+        int userId = intent.getIntExtra(CommunicationKeys.MEETING_ID, -1);
+
+        if (userId != -1) {
+            URI_UserBuilder uri_userBuilder = new URI_UserBuilder();
+            uri_userBuilder.addParameter(CommunicationKeys.USER_ID, Integer.toString(userId));
+
+            HttpAppClientGet httpAppClientGet = new HttpAppClientGet();
+            httpAppClientGet.setUri(uri_userBuilder.getURI());
+
+            try {
+                // TODO catch 404 (No Internet and Request Time out)
+                closeableHttpResponse = httpAppClientGet.executeRequest();
+            } catch (IOException e) {
+                // TODO handle Exception Toast? Alert Dialog? sent it to the Activity?
+            }
+
+            // accepted
+            try {
+                jasonString = EntityUtils.toString(closeableHttpResponse.getEntity());
+            } catch (Throwable e) {
+                // TODO handle Exception "can not Convert EntitlyUtils to String"
+            }
+
+            bundle.putString(edu.kit.pse.client.goapp.CommunicationKeys.USER, jasonString);
+
+            // send the Bundle and the Status Code from Response
+            resultReceiver.send(closeableHttpResponse.getStatusLine().getStatusCode(), bundle);
+        } else {
+            // Send a empty result with 500 as StatusCode
+
+            // StatusCode 500 is an unexpected Error. Here no Meeting ID in Intent
+            resultReceiver.send(500, bundle);
+        }
+    }
+
+    private void doDelet(Intent intent) {
+        CloseableHttpResponse closeableHttpResponse = null;
+
+        final ResultReceiver resultReceiver = intent.getParcelableExtra(CommunicationKeys.RECEICER);
+
+        Bundle bundle = new Bundle();
+        bundle.putString(CommunicationKeys.COMMAND, CommunicationKeys.DELETE);
+        bundle.putString(CommunicationKeys.SERVICE, CommunicationKeys.FROM_USER_SERVICES);
+
+        // if there no User Id in the Extra returns -1
+        int userId = intent.getIntExtra(CommunicationKeys.USER_ID, -1);
+
+        if (userId != -1) {
+            URI_UserBuilder uri_userBuilder = new URI_UserBuilder();
+            uri_userBuilder.addParameter(CommunicationKeys.USER_ID, Integer.toString(userId));
+
+            HttpAppClientDelete httpAppClientDelete = new HttpAppClientDelete();
+            httpAppClientDelete.setUri(uri_userBuilder.getURI());
+
+            try {
+                // TODO catch 404 (No Internet and Request Time out)
+                closeableHttpResponse = httpAppClientDelete.executeRequest();
+            } catch (IOException e) {
+                // TODO handle Exception Toast? Alert Dialog? sent it to the Activity?
+            }
+
+            // send the Bundle and the Status Code from Response
+            resultReceiver.send(closeableHttpResponse.getStatusLine().getStatusCode(), bundle);
+        } else {
+            // Send a empty result with 500 as StatusCode
+
+            // StatusCode 500 is an unexpected Error. Here no Meeting ID in Intent
+            resultReceiver.send(500, bundle);
+        }
+
+    }
+
+    private void doPut(Intent intent) {
+        String userAsJsonString = null;
+        CloseableHttpResponse closeableHttpResponse = null;
+
+        final ResultReceiver resultReceiver = intent.getParcelableExtra(CommunicationKeys.RECEICER);
+
+        Bundle bundle = new Bundle();
+        bundle.putString(CommunicationKeys.COMMAND, CommunicationKeys.PUT);
+        bundle.putString(CommunicationKeys.SERVICE, CommunicationKeys.FROM_USER_SERVICES);
+
+        userAsJsonString = intent.getStringExtra(CommunicationKeys.USER);
+
+        URI_UserBuilder uri_userBuilder = new URI_UserBuilder();
+
+        HttpAppClientPut httpAppClientPut = new HttpAppClientPut();
+        httpAppClientPut.setUri(uri_userBuilder.getURI());
+        try {
+            httpAppClientPut.setBody(userAsJsonString);
+        } catch (IOException e) {
+            //Todo Handle Exception. Maybe the String Extra was null
+        }
+
+        try {
+            // TODO catch 404 (No Internet and Request Time out)
+            closeableHttpResponse = httpAppClientPut.executeRequest();
+        } catch (IOException e) {
+            // TODO handle Exception Toast? Alert Dialog? sent it to the Activity?
+        }
+
+        // send the Bundle and the Status Code from Response
+        resultReceiver.send(closeableHttpResponse.getStatusLine().getStatusCode(), bundle);
+    }
+
+    private void doPost(Intent intent) {
+        String userAsJsonString = null;
+        CloseableHttpResponse closeableHttpResponse = null;
+
+        final ResultReceiver resultReceiver = intent.getParcelableExtra(CommunicationKeys.RECEICER);
+
+        Bundle bundle = new Bundle();
+        bundle.putString(CommunicationKeys.COMMAND, CommunicationKeys.POST);
+        bundle.putString(CommunicationKeys.SERVICE, CommunicationKeys.FROM_USER_SERVICES);
+
+        userAsJsonString = intent.getStringExtra(CommunicationKeys.USER);
+
+        URI_UserBuilder uri_userBuilder = new URI_UserBuilder();
+
+        HttpAppClientPost httpAppClientPost = new HttpAppClientPost();
+        httpAppClientPost.setUri(uri_userBuilder.getURI());
+        try {
+            httpAppClientPost.setBody(userAsJsonString);
+        } catch (IOException e) {
+            //Todo Handle Exception. Maybe the String Extra was null
+        }
+
+        try {
+            // TODO catch 404 (No Internet and Request Time out)
+            closeableHttpResponse = httpAppClientPost.executeRequest();
+        } catch (IOException e) {
+            // TODO handle Exception Toast? Alert Dialog? sent it to the Activity?
+        }
+
+        // send the Bundle and the Status Code from Response
+        resultReceiver.send(closeableHttpResponse.getStatusLine().getStatusCode(), bundle);
+    }
 }
