b732d8b12275c3b34af6b1e5be9e9798eb9cb2cd
Paula Klein <kleinpaula@ymail.com>
2016-07-10 16:02:05 +0200

    fertig

--- Implementierung/Client/GoApp/app/src/main/java/edu/kit/pse/client/goapp/service/UsersService.java
+++ Implementierung/Client/GoApp/app/src/main/java/edu/kit/pse/client/goapp/service/UsersService.java
@@ -1,7 +1,86 @@
 package edu.kit.pse.client.goapp.service;
 
+import android.app.IntentService;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.ResultReceiver;
+
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+
+import edu.kit.pse.client.goapp.CommunicationKeys;
+import edu.kit.pse.client.goapp.httpappclient.HttpAppClientGet;
+import edu.kit.pse.client.goapp.uri_builder.URI_MeetingBuilder;
+import edu.kit.pse.client.goapp.uri_builder.URI_UsersBuilder;
+
 /**
  * Created by paula on 10.07.16.
  */
-public class UsersService {
+public class UsersService extends IntentService {
+
+    //Konstruktor gibt den Service ein Namen, der fürs Testen wichtig ist.
+
+    public UsersService() {
+        super("UsersService");
+    }
+
+    /**
+     * Creates an IntentService.  Invoked by your subclass's constructor.
+     *
+     * @param name Used to name the worker thread, important only for debugging.
+     */
+    public UsersService(String name) {
+        super(name);
+    }
+
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        String command = intent.getStringExtra(CommunicationKeys.COMMAND);
+        switch (command) {
+            case "GET":
+                doGet(intent);
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void doGet (Intent intent) //throws  IOException
+    {
+        String jasonString = null;
+        CloseableHttpResponse closeableHttpResponse = null;
+
+        final ResultReceiver resultReceiver = intent.getParcelableExtra(CommunicationKeys.RECEICER);
+
+        Bundle bundle = new Bundle();
+        bundle.putString(CommunicationKeys.COMMAND, CommunicationKeys.GET);
+        bundle.putString(CommunicationKeys.SERVICE, CommunicationKeys.FROM_USERS_SERVICES);
+
+        URI_UsersBuilder uri_meetingBuilder = new URI_UsersBuilder();
+
+        HttpAppClientGet httpAppClientGet = new HttpAppClientGet();
+        httpAppClientGet.setUri(uri_meetingBuilder.getURI());
+
+        try {
+            // TODO catch 404 (No Internet and Request Time out)
+            closeableHttpResponse = httpAppClientGet.executeRequest();
+        } catch (IOException e) {
+            // TODO handle Exception Toast? Alert Dialog? sent it to the Activity?
+        }
+
+        // accepted
+        try {
+            jasonString = EntityUtils.toString(closeableHttpResponse.getEntity());
+        } catch (Throwable e) {
+            // TODO handle Exception "can not Convert EntitlyUtils to String"
+        }
+
+        bundle.putString(CommunicationKeys.USERS, jasonString);
+
+        // send the Bundle and the Status Code from Response
+        resultReceiver.send(closeableHttpResponse.getStatusLine().getStatusCode(), bundle);
+    }
 }
