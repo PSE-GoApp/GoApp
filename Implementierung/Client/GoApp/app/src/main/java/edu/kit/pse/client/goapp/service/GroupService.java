de03f6b09df8b91a7de89c8789614792575caa1b
Paula Klein <kleinpaula@ymail.com>
2016-07-10 16:44:00 +0200

    fertig

--- Implementierung/Client/GoApp/app/src/main/java/edu/kit/pse/client/goapp/service/GroupService.java
+++ Implementierung/Client/GoApp/app/src/main/java/edu/kit/pse/client/goapp/service/GroupService.java
@@ -6,17 +6,29 @@ import android.os.Bundle;
 import android.os.ResultReceiver;
 import android.util.Log;
 
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 import edu.kit.pse.client.goapp.CommunicationKeys;
 import edu.kit.pse.client.goapp.datamodels.Group;
+import edu.kit.pse.client.goapp.httpappclient.HttpAppClientDelete;
+import edu.kit.pse.client.goapp.httpappclient.HttpAppClientGet;
+import edu.kit.pse.client.goapp.httpappclient.HttpAppClientPost;
+import edu.kit.pse.client.goapp.httpappclient.HttpAppClientPut;
 import edu.kit.pse.client.goapp.parcelableAdapters.ParcelableGroup;
+import edu.kit.pse.client.goapp.uri_builder.URI_GroupBuilder;
+import edu.kit.pse.client.goapp.uri_builder.URI_MeetingBuilder;
 
 /**
  * Created by e6420 on 3.7.2016 г..
  */
 public class GroupService extends IntentService {
+
+    //Konstruktor gibt den Service ein Namen, der fürs Testen wichtig ist
 
     public GroupService() {
         super("GroupService");
@@ -36,21 +48,173 @@ public class GroupService extends IntentService {
         String command = intent.getStringExtra(CommunicationKeys.COMMAND);
         switch (command) {
             case "GET":
+                doGet(intent);
                 break;
             case "DELETE":
-                final ResultReceiver receiver = intent.getParcelableExtra(CommunicationKeys.RECEICER);
-                Bundle b = new Bundle();
-                b.putString(CommunicationKeys.COMMAND, "DELETE");
-                b.putString(CommunicationKeys.SERVICE, "GroupService");
-                receiver.send(202, b);
+                doDelete(intent);
                 break;
             case "PUT":
+                doPut(intent);
                 break;
             case "POST":
+                doPost(intent);
                 break;
             default:
                 break;
         }
 
     }
+
+    private void doGet (Intent intent) //throws  IOException
+    {
+        String jasonString = null;
+        CloseableHttpResponse closeableHttpResponse = null;
+
+        final ResultReceiver resultReceiver = intent.getParcelableExtra(CommunicationKeys.RECEICER);
+
+        Bundle bundle = new Bundle();
+        bundle.putString(CommunicationKeys.COMMAND, CommunicationKeys.GET);
+        bundle.putString(CommunicationKeys.SERVICE, CommunicationKeys.FROM_GROUP_SERVICES);
+
+        // if there no Group Id in the Extra returns -1
+        int groupId = intent.getIntExtra(CommunicationKeys.GROUP_ID, -1);
+
+        if (groupId != -1) {
+            URI_GroupBuilder uri_groupBuilder = new URI_GroupBuilder();
+            uri_groupBuilder.addParameter(CommunicationKeys.GROUP_ID, Integer.toString(groupId));
+
+            HttpAppClientGet httpAppClientGet = new HttpAppClientGet();
+            httpAppClientGet.setUri(uri_groupBuilder.getURI());
+
+            try {
+                // TODO catch 404 (No Internet and Request Time out)
+                closeableHttpResponse = httpAppClientGet.executeRequest();
+            } catch (IOException e) {
+                // TODO handle Exception Toast? Alert Dialog? sent it to the Activity?
+            }
+
+            // accepted
+            try {
+                jasonString = EntityUtils.toString(closeableHttpResponse.getEntity());
+            } catch (Throwable e) {
+                // TODO handle Exception "can not Convert EntitlyUtils to String"
+            }
+
+            bundle.putString(CommunicationKeys.GROUP, jasonString);
+
+            // send the Bundle and the Status Code from Response
+            resultReceiver.send(closeableHttpResponse.getStatusLine().getStatusCode(), bundle);
+        } else {
+            // Send a empty result with 500 as StatusCode
+
+            // StatusCode 500 is an unexpected Error. Here no Meeting ID in Intent
+            resultReceiver.send(500, bundle);
+        }
+    }
+
+
+    private void doDelete(Intent intent) {
+        CloseableHttpResponse closeableHttpResponse = null;
+
+        final ResultReceiver resultReceiver = intent.getParcelableExtra(CommunicationKeys.RECEICER);
+
+        Bundle bundle = new Bundle();
+        bundle.putString(CommunicationKeys.COMMAND, CommunicationKeys.DELETE);
+        bundle.putString(CommunicationKeys.SERVICE, CommunicationKeys.FROM_GROUP_SERVICES);
+
+        // if there no Meeting Id in the Extra returns -1
+        int groupId = intent.getIntExtra(CommunicationKeys.GROUP_ID, -1);
+
+        if (groupId != -1) {
+            URI_GroupBuilder uri_groupBuilder = new URI_GroupBuilder();
+            uri_groupBuilder.addParameter(CommunicationKeys.MEETING_ID, Integer.toString(groupId));
+
+            HttpAppClientDelete httpAppClientDelete = new HttpAppClientDelete();
+            httpAppClientDelete.setUri(uri_groupBuilder.getURI());
+
+            try {
+                // TODO catch 404 (No Internet and Request Time out)
+                closeableHttpResponse = httpAppClientDelete.executeRequest();
+            } catch (IOException e) {
+                // TODO handle Exception Toast? Alert Dialog? sent it to the Activity?
+            }
+
+            // send the Bundle and the Status Code from Response
+            resultReceiver.send(closeableHttpResponse.getStatusLine().getStatusCode(), bundle);
+        } else {
+            // Send a empty result with 500 as StatusCode
+
+            // StatusCode 500 is an unexpected Error. Here no Meeting ID in Intent
+            resultReceiver.send(500, bundle);
+        }
+
+    }
+
+    private void doPut(Intent intent) {
+        String groupAsJsonString = null;
+        CloseableHttpResponse closeableHttpResponse = null;
+
+        final ResultReceiver resultReceiver = intent.getParcelableExtra(CommunicationKeys.RECEICER);
+
+        Bundle bundle = new Bundle();
+        bundle.putString(CommunicationKeys.COMMAND, CommunicationKeys.PUT);
+        bundle.putString(CommunicationKeys.SERVICE, CommunicationKeys.FROM_GROUP_SERVICES);
+
+        groupAsJsonString = intent.getStringExtra(CommunicationKeys.GROUP);
+
+        URI_GroupBuilder uri_groupBuilder = new URI_GroupBuilder();
+
+        HttpAppClientPut httpAppClientPut = new HttpAppClientPut();
+        httpAppClientPut.setUri(uri_groupBuilder.getURI());
+        try {
+            httpAppClientPut.setBody(groupAsJsonString);
+        } catch (IOException e) {
+            //Todo Handle Exception. Maybe the String Extra was null
+        }
+
+        try {
+            // TODO catch 404 (No Internet and Request Time out)
+            closeableHttpResponse = httpAppClientPut.executeRequest();
+        } catch (IOException e) {
+            // TODO handle Exception Toast? Alert Dialog? sent it to the Activity?
+        }
+
+        // send the Bundle and the Status Code from Response
+        resultReceiver.send(closeableHttpResponse.getStatusLine().getStatusCode(), bundle);
+    }
+
+    private void doPost(Intent intent) {
+        String groupAsJsonString = null;
+        CloseableHttpResponse closeableHttpResponse = null;
+
+        final ResultReceiver resultReceiver = intent.getParcelableExtra(CommunicationKeys.RECEICER);
+
+        Bundle bundle = new Bundle();
+        bundle.putString(CommunicationKeys.COMMAND, CommunicationKeys.POST);
+        bundle.putString(CommunicationKeys.SERVICE, CommunicationKeys.FROM_GROUP_SERVICES);
+
+        groupAsJsonString = intent.getStringExtra(CommunicationKeys.GROUP);
+
+        URI_GroupBuilder uri_groupBuilder = new URI_GroupBuilder();
+
+        HttpAppClientPost httpAppClientPost = new HttpAppClientPost();
+        httpAppClientPost.setUri(uri_groupBuilder.getURI());
+        try {
+            httpAppClientPost.setBody(groupAsJsonString);
+        } catch (IOException e) {
+            //Todo Handle Exception. Maybe the String Extra was null
+        }
+
+        try {
+            // TODO catch 404 (No Internet and Request Time out)
+            closeableHttpResponse = httpAppClientPost.executeRequest();
+        } catch (IOException e) {
+            // TODO handle Exception Toast? Alert Dialog? sent it to the Activity?
+        }
+
+        // send the Bundle and the Status Code from Response
+        resultReceiver.send(closeableHttpResponse.getStatusLine().getStatusCode(), bundle);
+    }
+
 }
+
