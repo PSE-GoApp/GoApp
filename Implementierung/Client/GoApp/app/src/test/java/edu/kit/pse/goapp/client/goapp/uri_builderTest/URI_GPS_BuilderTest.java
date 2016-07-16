5c47d171c7a73933a94452acc19d0985c3202752
Paula Klein <kleinpaula@ymail.com>
2016-07-16 18:46:34 +0200

    tests for URI_Builder

---
+++ Implementierung/Client/GoApp/app/src/test/java/edu/kit/pse/goapp/client/goapp/uri_builderTest/URI_GPS_BuilderTest.java
@@ -0,0 +1,41 @@
+package edu.kit.pse.goapp.client.goapp.uri_builderTest;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import java.net.URI;
+
+import edu.kit.pse.client.goapp.uri_builder.URI_GPS_Builder;
+
+/**
+ * Created by paula on 15.07.16.
+ */
+public class URI_GPS_BuilderTest {
+
+    @Test
+    public void builderTest() {
+        URI_GPS_Builder builder = new URI_GPS_Builder();
+        URI uri = builder.getURI();
+        URI testUri = null;
+        try {
+            testUri = new URI("http://localhost:8080/GoAppServer/Gps");
+        } catch (java.net.URISyntaxException e){
+            //cant happen
+        }
+        assertEquals(testUri,uri);
+    }
+
+    @Test
+    public void addParameterTest() {
+        URI_GPS_Builder builder = new URI_GPS_Builder();
+        builder.addParameter("testKey","testValue");
+        URI uri = builder.getURI();
+        URI testUri= null;
+        try {
+            testUri = new URI("http://localhost:8080/GoAppServer/Gps?testKey=testValue");
+        } catch (java.net.URISyntaxException e){
+            //cant happen
+        }
+        assertEquals(testUri,uri);
+    }
+}
