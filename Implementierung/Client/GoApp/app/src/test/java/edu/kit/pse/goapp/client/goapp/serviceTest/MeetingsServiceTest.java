4662640401cb20f899fb577436527e6f5a5273e9
Paula Klein <kleinpaula@ymail.com>
2016-08-18 15:48:25 +0200

    finished

---
+++ Implementierung/Client/GoApp/app/src/test/java/edu/kit/pse/goapp/client/goapp/serviceTest/MeetingsServiceTest.java
@@ -0,0 +1,47 @@
+package edu.kit.pse.goapp.client.goapp.serviceTest;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import edu.kit.pse.client.goapp.CommunicationKeys;
+import edu.kit.pse.client.goapp.ServiceResultReceiver;
+import edu.kit.pse.client.goapp.service.MeetingsService;
+
+/**
+ * Created by paula on 18.08.16.
+ */
+public class MeetingsServiceTest extends AppCompatActivity implements ServiceResultReceiver.Receiver{
+
+    Intent intent;
+    int resultCode;
+    Bundle resultBundle;
+    ServiceResultReceiver receiver;
+
+
+
+    @Before
+    public void initializeTest() {
+        intent = new Intent(this, MeetingsService.class);
+        receiver = new ServiceResultReceiver(new Handler());
+        receiver.setReceiver(this);
+        intent.putExtra(CommunicationKeys.RECEICER, receiver);
+        resultCode = -1;
+    }
+
+    @Test
+    public void getTest() {
+        intent.putExtra(CommunicationKeys.COMMAND, CommunicationKeys.GET);
+        startService(intent);
+    }
+
+    @Override
+    public void onReceiveResult(int resultCode, Bundle resultData) {
+        this.resultCode = resultCode;
+        this.resultBundle = resultData;
+    }
+}
